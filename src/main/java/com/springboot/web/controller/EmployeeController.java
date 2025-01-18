716ff477a569080685b167d69e2db6dec8f9789c
lipu150 <jayakrushnarout100@gmail.com>
2025-01-18 17:31:42 +0530

    api created

---
+++ src/main/java/com/springboot/web/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.springboot.web.controller;
+
+import com.springboot.web.model.Employee;
+import com.springboot.web.service.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("employee")
+public class EmployeeController
+{
+    @Autowired
+    private EmployeeServiceImpl service;
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<?> save(@RequestBody Employee employee)
+    {
+        Employee emp=service.save(employee);
+        if(emp!=null)
+        {
+            return new ResponseEntity<>(emp, HttpStatus.CREATED);
+        }
+        else
+        {
+            return new ResponseEntity<String>("Enter valid Employee",HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @GetMapping("/getEmployee/{id}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable Integer id)
+    {
+        return new ResponseEntity<>(service.getEmployeeById(id),HttpStatus.OK);
+    }
+//employee/getAll
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAllEmployees() {
+        return new ResponseEntity<>(service.getAllEmployees(),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<?> deltebyId(@PathVariable Integer id) {
+
+        return new ResponseEntity<>("deleted successfully",HttpStatus.OK);
+    }
+}
